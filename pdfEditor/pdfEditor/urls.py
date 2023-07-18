0e3e86aea6912bd49cb10d0809959c316b34000b
Amalhamdi007 <amal@24eme.fr>
2023-07-18 17:31:23 +0200

    fix download pdf after add image

--- pdfEditor/pdfEditor/urls.py
+++ pdfEditor/pdfEditor/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path
 from pdfEditorApp.views import download_compressed, compressPdf, displayPdf, lockPdf, unlockPdf, addImage
@@ -10,15 +9,10 @@ urlpatterns = [
     path('displayPdf/', displayPdf, name='displayPdf'),
     path('addImage/', addImage, name='addImage'),
     path('deletePagePdf/', deletePagePdf, name='deletePagePdf'),
-
     path('compressPdf/', compressPdf, name='compressPdf'),
     path('download-compressed/', download_compressed, name='download_compressed'),
-
-
-
     path('unlockPdf/', unlockPdf, name='unlockPdf'),
     path('download_unlocked_file', download_unlocked_file, name='download_unlocked_file'),
-
     path('lockPdf/', lockPdf, name='lockPdf'),
     path('download_locked_file', download_locked_file, name='download_locked_file'),
 ]
