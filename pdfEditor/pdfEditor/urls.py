fd062209f81f39e45394b79c9022fa6c71908797
bahfatma <131278061+bahfatma@users.noreply.github.com>
2023-07-20 16:57:03 +0200

    Update urls.py

--- pdfEditor/pdfEditor/urls.py
+++ pdfEditor/pdfEditor/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from pdfEditorApp.views import download_compressed, compressPdf, displayPdf, lockPdf, unlockPdf, addImage
 
 from pdfEditorApp.views import fillFormPdf, appendPdf, download_append_file, splitPdf
-from pdfEditorApp.views import download_locked_file, download_unlocked_file, homePagePdf,deletePagePdf
+from pdfEditorApp.views import homePagePdf,deletePagePdf
 
 
 urlpatterns = [
@@ -24,7 +24,6 @@ urlpatterns = [
     path('unlockPdf/', unlockPdf, name='unlockPdf'),
     path('lockPdf/', lockPdf, name='lockPdf'),
     path('splitPdf/', splitPdf, name='splitPdf'),
-    path('download_locked_file', download_locked_file, name='download_locked_file'),
     #path('modifyText/', pdfeditor, name='modifyText'),
 ]
 
